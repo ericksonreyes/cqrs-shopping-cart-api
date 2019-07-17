ca68d4822eeb92ae88719d79bb9477f3485f47f1
Sourcefit <sourcefit@Ericksons-MacBook-Air.local>
2019-07-17 14:42:14 +0800

    Add a product listing API end point

--- index.js
+++ index.js
@@ -6,11 +6,48 @@ const app = express()
 const port = 3000
 const appSecret = 'secret-word'
 
+const products = [
+    {
+        "id": "product-1",
+        "name": "Air Jordan 11",
+        "price": 1132
+    },
+    {
+        "id": "product-2",
+        "name": "Air Qatar 11",
+        "price": 879
+    },
+    {
+        "id": "product-i1",
+        "name": "Air Saudi Arabia 2",
+        "price": 150
+    },
+    {
+        "id": "product-i1",
+        "name": "Air Kuwait 12",
+        "price": 212
+    }
+]
+
 app.use(cors())
 app.use(express.json())
-app.use(jwtReader({secret: appSecret}).unless({path: ['/v1/api/auth']}));
+app.use(
+    jwtReader({secret: appSecret})
+        .unless(
+            {
+                path: [
+                    '/v1/api/auth',
+                    '/v1/api/products',
+                    '/v1/api/products/:id',
+                ]
+            }
+        )
+);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
+/**
+ * ROUTES
+ */
 app.get('/v1/api', (req, res) => {
     res.json({'response': 'Hello World!'})
 })
@@ -21,7 +58,7 @@ app.post('/v1/api/auth', (req, res) => {
         const token = jwt.sign({
             exp: Math.floor(Date.now() / 1000) + (60 * 60),
             data: {
-                id : 'customer-1'
+                id: 'customer-1'
             }
         }, appSecret);
         res.json({'accessToken': token})
@@ -39,4 +76,8 @@ app.post('/v1/api/auth', (req, res) => {
             ]
         }
     );
+})
+
+app.get('/v1/api/products', (req, res) => {
+    res.json(products);
 })
